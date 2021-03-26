5fd3546ad507b0ef2a9a6a52fe778ac16039f078
Matthew Lee <matthew.lee-mattner@griffithuni.edu.au>
2021-03-26 14:33:33 +1000

    Objects and transformations

--- main.js
+++ main.js
@@ -7,20 +7,72 @@ renderer.setClearColor( 0xffffff, 1 );
 document.body.appendChild( renderer.domElement );
 
 
-const square = createTriangle(3, color=0xffffff);
+//const square = createTriangle(3, color=0x000000);
+//var s = createSquare(3, color=0x00ff00);
 
 //const arr = createArrow(new THREE.Vector3(2, 1, 0), new THREE.Vector3(-2, 3, 0));
-const arr = createArrow(new THREE.Vector3(-2, 3, 0), new THREE.Vector3(2, 1, 0));
-scene.add(square);
-scene.add(arr);
+//var arr = createArrow(new THREE.Vector3(-2, 3, 0), new THREE.Vector3(2, 1, 0));
+//scene.add(s)
+//scene.add(square);
+//scene.add(arr);
+var halfWidth = 1;
+var halfHeight = 1;
+var vertices = [
+  new THREE.Vector3(-1 * halfWidth, 1 * halfHeight, 0),
+  new THREE.Vector3(1 * halfWidth, 1 * halfHeight, 0),
+  new THREE.Vector3(1 * halfWidth, -1 * halfHeight, 0),
+  new THREE.Vector3(-1 * halfWidth, -1 * halfHeight, 0)
+]
+
+var obj = new Polygon(vertices, {color: 0x00ff99, strokeColor: 0x666});
+var translation = document.getElementById("translate");
+var rotation = document.getElementById("rotation");
+var scale = document.getElementById("scale");
+
+function setVertex(object, index, vector){
+  var pArr = object.geometry.attributes.position.array;
+  pArr[index * 3] = vector.x;
+  pArr[index * 3 + 1] = vector.y;
+  pArr[index * 3 + 2] = vector.z;
+  object.geometry.attributes.position.needsUpdate = true;
+}
+
+function translateVector(object, index, translateVector){
+  var pArr = object.geometry.attributes.position.array;
+  pArr[index * 3] += translateVector.x;
+  pArr[index * 3 + 1] += translateVector.y;
+  pArr[index * 3 + 2] += translateVector.z;
+  object.geometry.attributes.position.needsUpdate = true;
+}
+
+function moveArrow(arr, startVertex, endVertex){
+  var arr2 = createArrow(startVertex, endVertex);
+  arr.children[0].geometry = arr2.children[0].geometry;
+  arr.children[1] = arr2.children[1];
+  return arr;
+}
 
 camera.position.z = 5;
-
 const animate = function () {
   requestAnimationFrame( animate );
 
+  obj.positionX(translation.value);
+  obj.rotateZ(-rotation.value)
+  obj.scaleY(scale.value);
+  /*
   square.rotateZ(0.01);
 
+
+
+  //s.rotation.z = slider.value;
+
+  s.geometry.attributes.position.array[0] = slider.value;
+
+  arr = moveArrow(arr, new THREE.Vector3(-2, 3, 0),
+  new THREE.Vector3(s.geometry.attributes.position.array[0], s.geometry.attributes.position.array[1], 0));
+  s.geometry.attributes.position.needsUpdate = true;
+  */
+  //arr.children[1].rotation.z = slider.value;
   renderer.render( scene, camera );
 };
 
--- minput/minput.js
+++ minput/minput.js
@@ -1,3 +1,130 @@
+
+class Mtransform{
+  positionX(value){
+    this.group.position.x = value;
+  }
+
+  positionY(value){
+    this.group.position.y = value;
+  }
+
+  positionZ(value){
+    this.group.position.z = value;
+  }
+
+  position(vector){
+    this.positionX(vector.x);
+    this.positionY(vector.y);
+    this.positionZ(vector.z);
+  }
+
+  scaleX(value){
+    this.group.scale.x = value;
+  }
+
+  scaleY(value){
+    this.group.scale.y = value;
+  }
+
+  scaleZ(value){
+    this.group.scale.z = value;
+  }
+
+  scale(vector){
+    this.scaleX(vector.x);
+    this.scaleY(vector.y);
+    this.scaleZ(vector.z);
+  }
+
+  rotateX(value){
+    this.group.rotation.x = value;
+  }
+
+  rotateY(value){
+    this.group.rotation.y = value;
+  }
+
+  rotateZ(value){
+    this.group.rotation.z = value;
+  }
+
+  rotate(vector){
+    this.rotateX(vector.x);
+    this.rotateY(vector.y);
+    this.rotateZ(vector.z);
+  }
+}
+
+class Mobject extends Mtransform{
+  mesh;
+  shape;
+  geometry;
+  border;
+  group;
+  material;
+  vertices;
+  color;
+  strokeColor;
+  strokeWidth;
+
+  constructor(vertices, {color=0x000, strokeColor=0x000, strokeWidth=0.1} = {}){
+    super();
+    this.vertices = vertices;
+    this.color = color;
+    this.strokeColor = strokeColor;
+    this.strokeWidth = strokeWidth;
+
+    this.create();
+  }
+
+  createGeometry(shape){
+    return new THREE.ShapeGeometry(shape);
+  }
+
+  createBorder(){
+    var borderVerts = this.vertices;
+    borderVerts.push(borderVerts[0])
+    var line = new MeshLine();
+    line.setPoints(borderVerts, p => this.strokeWidth);
+    var material = new MeshLineMaterial({color: this.strokeColor});
+    this.border = new THREE.Mesh(line, material);
+    return this.border;
+  }
+
+  create(addToScene=true){
+    this.shape = new THREE.Shape(this.vertices);
+    this.geometry = this.createGeometry(this.shape);
+    this.material = new THREE.MeshBasicMaterial({color: this.color});
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+
+    this.border = this.createBorder();
+    this.group = new THREE.Group();
+    this.group.add(this.mesh);
+    this.group.add(this.border);
+    if(addToScene){
+      scene.add(this.group);
+    }
+    return this.group;
+  }
+
+  update(){
+    var shape = new THREE.Shape(this.vertices);
+    this.geometry = this.createGeometry(shape);
+
+    var newBorder = this.createBorder();
+    this.border.geometry = newBorder.geometry;
+
+    this.geometry.attributes.position.needsUpdate = true;
+    this.border.geometry.attributes.position.needsUpdate = true;
+  }
+}
+
+class Polygon extends Mobject {
+  contructor(vertices, {color=0x000, strokeColor=0x000, strokeWidth=0.1} = {}){
+    super(vertices);
+  }
+}
+
 function createLine(vertices, width=0.1, color=0x000){
   const line = new MeshLine();
   line.setPoints(vertices, p => width);
@@ -5,33 +132,35 @@ function createLine(vertices, width=0.1, color=0x000){
   return new THREE.Mesh(line, material);
 }
 
-function createArrow(startVertex, endVertex, width=0.1, headWidth=0.5, color=0x000){
-  const vertices = [startVertex, endVertex];
-  const line = createLine(vertices, width, color);
-  const arrowHead = createTriangle(headWidth, color=color);
+function createArrow(startVertex, endVertex, width=0.03, headWidth=0.15, color=0x000){
 
-  var arrowPos = new THREE.Vector3(0, 1, 0);
-
-
-
-
-
-  if(startVertex.x > endVertex.x){
-    var endVertexCopy = new THREE.Vector3(0, 0, 0)
-    endVertexCopy.copy(endVertex);
-    var direction = endVertexCopy.sub(startVertex);
-    var angle = direction.angleTo(arrowPos);
-  }else{
-    var direction = startVertex.sub(endVertex);
-    var angle = arrowPos.angleTo(direction);
-
+  var dxdy = new THREE.Vector3(endVertex.x - startVertex.x, endVertex.y - startVertex.y, endVertex.z - startVertex.z);
+  var dist = startVertex.distanceTo(endVertex);
+  if(dist > 0){
+    dxdy.x /= dist;
+    dxdy.y /= dist;
   }
+  dxdy.x *= dist - headWidth;
+  dxdy.y *= dist - headWidth;
+  endVertex = new THREE.Vector3(startVertex.x + dxdy.x, startVertex.y + dxdy.y, 0);
+  var vertices = [startVertex, endVertex];
+  var line = createLine(vertices, width, color);
+  var arrowHead = createTriangle(headWidth, color=color);
 
 
+  //console.log(dist);
+  // Calculate the angle between points
+  arrowHead.translateX(endVertex.x);
+  arrowHead.translateY(endVertex.y);
 
-  arrowHead.position.set(endVertex.x, endVertex.y, endVertex.z);
+  var v1 = endVertex.sub(startVertex);
+  var v2 = new THREE.Vector3(0, 1, 0);
+  var angle = v2.angleTo(v1);
+  var orientation = v1.x * v2.y - v1.y * v2.x;
+  if(orientation > 0) angle = 2*Math.PI - angle;
+
   arrowHead.rotateZ(angle);
-  console.log(angle)
+
   const group = new THREE.Group();
   group.add(line);
   group.add(arrowHead);
@@ -47,10 +176,11 @@ function createPolygon(vertices, color=0x1b87e5, strokeColor=0x000000, strokeWid
   vertices.push(vertices[0]);
   const border = createLine(vertices, width=strokeWidth, color=strokeColor);
   const group = new THREE.Group();
-  group.add(border);
-  group.add(shape);
 
-  return group;
+  group.add(shape);
+  //group.add(border);
+
+  return shape;
 }
 
 function createRectangle(width=2, height=1, color=0x1b87e5){
@@ -83,9 +213,10 @@ function createCircle(diameter=1, steps=50, color=0x1b87e5){
 function createTriangle(size=1, color=0x1b87e5){
   var halfSize = size / 2;
   var vertices = [
-    new THREE.Vector3(-1 * halfSize, -1 * halfSize, 0),
-    new THREE.Vector3(0, 2 * halfSize, 0),
-    new THREE.Vector3(1 * halfSize, -1 * halfSize, 0),
+    new THREE.Vector3(-1 * halfSize, 0, 0),
+    new THREE.Vector3(0, size, 0),
+    new THREE.Vector3(1 * halfSize, 0, 0),
   ]
-  return createPolygon(vertices, color);
+  var triangle = createPolygon(vertices, color);
+  return triangle;
 }
